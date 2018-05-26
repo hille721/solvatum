2d2e1181572dcd67023e7a2075b335600924f206
Christoph Hille <c.hille@tum.de>
2018-05-26 13:57:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,114 @@
+This solvation free energy database contains partition coefficients (logK)
+of 658 neutrale organic molekules and inorganic gases dissolved in organic solvents.
+The values were provided by Prof. Dr. Acree Jr.
+All publications of these values (36 in total) are listed in the [bib file](references/references.bib).
+The [database](database/database.sdf) also contains in vacuum optimized geometries for each solute.
+
+For a simple and fast access to the data one can use the database interface 
+([databasehandler](database/databasehandler.py)), which was written in the Python programming language.
+
+The installation and usage of this database interface is described in the following sections.
+
+# Installation:
+
+## Getting the repository:
+    
+    $ git clone https://gitlab.lrz.de/hille721/solvation-free-energy-database
+    
+## Installation of Pybel/OpenBabel
+
+For using the database interface one need to install Pybel and also OpenBabel.
+Installation guidelines can be found here: 
+
+* [Pybel](https://openbabel.org/docs/dev/UseTheLibrary/Python_Pybel.html)
+* [OpenBabel](http://openbabel.org/wiki/Main_Page) 
+
+We recommend to install Pybel/OpenBabel with the conda package manager,
+because then no further installations are necessary.
+For this just type 
+    
+    $ conda install -c openbabel openbabel
+
+in your command line.
+(This requires of course that conda is installed)
+
+  
+# Usage of the database interface:
+
+For using the database interface from each directory, ad the pyhtonpath to your bashrc:
+
+```bash
+export PYTHONPATH=$PYTHONPATH:<path_to_the_repository>/solvation-free-energy-database/database
+``` 
+
+Open a Python prompt and type in:
+
+```python
+>>> from databasehandler import Database
+>>> d = Database()
+```
+
+With this command the database gets initialized.
+Now all solvents or solutes can be listed with:
+
+```python
+>>> print d.solvents
+>>> print d.solutes
+```
+
+One can filter for one solvent, solute or both:
+
+```python
+>>> d.filtering(solvent='chloroform')
+>>> d.filtering(solute='hexane')
+>>> d.filtering(solvent='chloroform', solute='hexane')
+{'deltaG_solv': -0.16979, 'logK': 2.87}
+
+```
+The output of the first lines is a dictionary with molecule specific propteries
+and a list with all corresponding solutes and solvents, respectively.
+The output of the latter, is a dictionary with the logK value 
+and the solvation free energy calculated from it.
+The unit of the solvation free energy is per default eV,
+but one can switch to kcal/mol with
+
+```python
+>>> d.energy_unit='kcal/mol'
+{'deltaG_solv': -3.92, 'logK': 2.87}
+```
+
+Adding new properties can simple done with
+
+```python
+>>> d.add_property(<molecule>, <property_name>, <property_value>)
+```    
+    
+For adding dielectric constants, there exists a extra command:
+
+```python
+>> d.add_dielectric_constant(<molecule>, <value>, <source>)
+```
+
+The database interface also provides the possibility to draw depictions of the molecules:
+
+```python
+>>> d.draw(<molecule>)
+```
+
+This method take the keywords 'save=True', for saving a png figure of the depiction and
+'notebook=True' for showing the depiction inline in jupyter notebooks.
+
+# Known Issues
+There is a known issue in Pybel in the drawing functionality, which was already reported and is also fixed in the Pybel source code.
+But it could be that with installing Pybel with conda or PIP and not directly from source the following error still appears:
+
+```python
+ImportError: Tkinter or Python Imaging Library not found, but is required for image display. See installation instructions for more information.
+```
+
+For this we provided a fixed version of the pybel.py source code. Just put the Pythonpath to the directory fixed_pybel in your bashrc:
+
+```bash
+export PYTHONPATH=$PYTHONPATH:<path_to_the_repository>/solvation-free-energy-database/fixed_pybel
+``` 
+
---
+++ database/databasehandler.py
@@ -0,0 +1,411 @@
+#!/usr/bin/python
+"""
+With this module you can interact with the database.
+(Call up values or possible (in meaning that there exists a logK value) solvent-solute combinations, change values, add properties, ...)
+
+Version 2
+Last Update 16.04.2018
+@author: Christoph Hille
+"""
+
+import sys
+import os
+from os.path import join as pathjoin
+import pybel
+import numpy as np
+from scipy import constants
+
+class Database:
+    """
+    options:
+        - path:         - str; path to the database
+        - name:         - str; databasename
+                          default: 'database.sdf'
+        - energy_unit:  - str; 'eV' or 'kcal'
+                          in which unit the energy will be given
+                          default: eV
+        - temperature:  - float; temperature (for energy calculation);
+                          default: 298.15
+    """
+    
+    def __init__(self, path=None,
+                       name='database.sdf',
+                       energy_unit='eV',
+                       temperature=298.15
+                       ):
+        
+        if path:
+            self.path = path
+        else:
+            self.path = os.path.dirname(__file__)
+
+        self.__sdf_file = list(pybel.readfile("sdf", pathjoin(self.path, name)))	    
+        self.solutes = self.__get_all_solutes()
+        self.solvents = self.__get_all_solvents()
+        self.energy_unit = energy_unit
+        self.temperature = temperature
+            
+    def __get_all_solutes(self):
+        """
+        Returns dictionary of all solute-molecules in the database with solute ID as key and solute name as values
+        """
+        
+        solutes = {}
+
+        for mol in self.__sdf_file:
+            solutes[mol.title] = mol.data['Name']
+	
+        return solutes
+
+    def __get_all_solvents(self):
+        """
+        Returns dictionary of all solvent-molecules in the database with the solvent name as key and a tuple of the number of 
+        solutes for which reference data exists and the dieelctric constant as values 
+        
+        Example:
+            {'METHANOL': (104, 32.613), ...}
+            There exists 104 reference values for Methanol as solvent and the dieelctric constant of Methanol is 32.613
+        """
+	
+        solvents = {}
+        eps = {}
+
+        for mol in self.__sdf_file:
+            data = mol.data
+            for name in data:
+                if 'logK' in name:
+                    sol = name[6:-1].upper()
+                elif 'WATER' in name:
+                    sol = 'WATER'
+                elif name == 'dielectric constant':
+                    eps[data['Name']] = data[name]
+                else:
+                    continue
+                if sol not in solvents:
+                    solvents[sol] = 1
+                else:
+                    solvents[sol] += 1
+        
+        solvents_out = {}
+        for sol in solvents:
+            if sol in eps:
+                solvents_out[sol] = (solvents[sol], eps[sol])
+            else:
+                solvents_out[sol] = (solvents[sol], None)
+
+        return solvents_out
+	
+    def __check_if_molecules_in_database(self, molecules_list, issolvent=False, disp=True):
+        """
+        Checks for every molecule in a given list if it has a geometry entry in the database
+        
+        I raises an error if a solute doesn't have an geometry entry (because thats means tha)
+        """	
+        if type(molecules_list) != list:
+            raise TypeError('Molecules list has to be a list!')
+        
+        for sol in molecules_list:
+            if sol not in self.solutes.keys() and sol.upper() not in self.solutes.values():
+                if not issolvent:
+                    raise KeyError('The given solute %s could not be identified as a solute in the database' %sol)
+                else:
+                    if sol.upper() not in self.solvents:
+                        raise KeyError('The given solvent %s could not be identified as a solvent in the database' %sol)
+                    if disp:
+                        print 'There is no geometry entry for solvent %s in database' %sol
+                    return 'no geometry'
+	
+    def __name_id_handler(self, molecules_list, direction='id', issolvent=False, disp=True):
+        """ 
+        Converts soluteID to solute name and vice versa
+        (eg:    INPUT: solutes_list=[Argon], direction='id' -> OUTPUT: solutes_list=[002]
+                INPUT: solutes_list=[081], direction='name'	-> OUTPUT: solutes_list=[METHANOL]
+        """
+        if self.__check_if_molecules_in_database(molecules_list, issolvent=issolvent, disp=disp) == 'no geometry':
+            return ['no geometry']
+        
+        if direction == 'id':
+            for i in range(len(molecules_list)):
+                if molecules_list[i] in self.solutes.keys():
+                    pass
+                else:
+                    molecules_list[i] = self.solutes.keys()[self.solutes.values().index(molecules_list[i].upper())]
+                
+        elif direction == 'name':
+            for i in range(len(molecules_list)):
+                if molecules_list[i].upper() in self.solutes.values():
+                    molecules_list[i] = molecules_list[i].upper()
+                else:
+                    molecules_list[i] = self.solutes[molecules_list[i]]
+
+        return molecules_list
+
+    def __filter_solvent(self, solvent, solutes='all', disp=True):
+        """
+        Returns dictionary with solvent properties and a list of soluteIDs, solutes and logK values for the given solvent
+        """
+        if solutes == 'all':
+            solutes = self.__sdf_file
+            
+        solvent = solvent.upper()
+        solventname = self.__name_id_handler([solvent], direction='name', issolvent=True, disp=disp)[0]
+        if solventname == 'no geometry':
+            nogeo = True
+        else:
+            nogeo = False
+            solvent = solventname
+        
+        if solvent not in self.solvents:
+            print 'Solvent %s was not found in the database' %solvent
+            return 
+
+        if solvent == 'WATER':
+            key = 'dgexp marzari (WATER)'
+        else:
+            key = 'logK (' + solvent + ')'
+        solutes_list = []
+        for mol in solutes:
+            if key in mol.data.keys():
+                if solvent == 'WATER':
+                    logK = None
+                    deltaGsolv = float(mol.data[key])
+                    if self.energy_unit == 'eV':
+                        deltaGsolv = round((deltaGsolv * constants.kilo * constants.calorie) / (constants.N_A * constants.eV), 4) #conversion kcal/mol to eV
+                else:  
+                    logK = float(mol.data[key])
+                    deltaGsolv =  self.__calculate_G_solv(logK)
+                solutes_list += [[mol.title, mol.data['Name'], logK, deltaGsolv]]
+
+        if nogeo:
+            props = {}
+        else:
+            props = self.__get_molecule_properties(solvent)
+            
+        return props, solutes_list
+
+    def __one_mol_from_sdf(self, solute):
+        """
+        Returns the pybel mol-object for a given solute (name or id)
+        """
+
+        if type(solute) != str:
+            raise TypeError('Solute has to be a single string')
+
+        solute = self.__name_id_handler([solute])[0]
+
+        return self.__sdf_file[int(solute)]
+
+    def __filter_solute(self, solute):
+        """
+        Returns a list of solvents and logK values for a given solute 
+        """
+	
+        mol = self.__one_mol_from_sdf(solute.upper())
+
+        solvents_list = []
+        for key in mol.data.keys():
+            if key[0:4] == 'logK':
+                solvents_list += [[key[6:-1 ], mol.data[key], self.__calculate_G_solv(eval(mol.data[key]))]]
+        if solute.upper() == 'WATER':
+            solvents_list +=[['WATER', None, mol.data['dgexp marzari (WATER)']]]
+        props = self.__get_molecule_properties(solute)
+
+        return props, solvents_list
+
+    def __filter_solvent_solute(self, solvent, solute, disp=True):
+        """
+        Returns the logK and solvation free energy value for a given solvent, solute combinaton
+        """
+
+        solutemol = [self.__one_mol_from_sdf(solute)]
+        
+        solvent_solute = self.__filter_solvent(solvent, solutes=solutemol, disp=disp)[1]
+        if len(solvent_solute) == 0:
+            raise KeyError('No logK value for solute %s and solvent %s was found in the database' %(solute, solvent)) 
+            
+        logK, deltaGsolv = solvent_solute[0][2:]
+        return {'logK': logK, 'deltaG_solv': deltaGsolv}
+
+    def __calculate_G_solv(self, logK):
+        """
+        Calculates the solvation free energy for a given logK value in kcal/mol or eV.
+        Formular: deltaGsolv = -ln(10)*RT*logK
+
+        INPUT:
+            - logK:     float; Partition Coefficient
+
+            optional:
+                - unit: str; 'kcal' or 'eV'; unit; default: 'eV'
+                        (by initianlization of the class object one can also specify the energy unit with 'energy_unit') 
+                - T:    float; temperature; default: 298.15
+        """  
+
+        unit = self.energy_unit
+        T = self.temperature
+
+        delta_G_solv = - constants.R * T * np.log(10) * logK #deltaGsolv in joule
+        
+        if unit == 'kcal/mol':
+            delta_G_solv = round(delta_G_solv / (constants.calorie * constants.kilo), 2) #conversion joule to kcal/mol
+        elif unit == 'eV':
+            delta_G_solv = round(delta_G_solv / (constants.N_A * constants.eV), 5) # conversion joule to eV
+        elif unit == 'J':
+            delta_G_solv = round(delta_G_solv, 2)
+        else:
+            raise IOError("The unit %s is not supported as energy unit in this database. Please choose between 'kcal/mol', 'eV' or 'J'" %unit)
+
+        return delta_G_solv
+
+    def __get_molecule_properties(self, solvent):
+
+        mol = self.__one_mol_from_sdf(solvent)
+
+        eps = 'dielectric constant'
+        eps_source = 'dielectric constant source'
+        gamma = 'surface tension'
+        polar = 'mean polarizability'
+        dipole = 'dipole moment'
+        polar_exp = 'experimental polarizability'
+
+        props = {}
+
+        props['molweight'] = round(float(mol.molwt),2)
+        props['formula'] = mol.formula
+        props['InChI'] = mol.data['InChI']
+        props['vacuum energy'] = mol.data['vacuum energy']
+        props['databaseID'] = mol.title
+
+        if eps in mol.data:
+            props[eps] =  float(mol.data[eps])
+        if eps_source in mol.data:
+            props[eps_source] =  mol.data[eps_source]
+        elif eps in mol.data:
+            print('There is no source for the dielectric constant value of %s' %solvent)
+        if gamma in mol.data:
+            props[gamma] = float(mol.data[gamma])
+        if polar in mol.data:
+            props[polar] = float(mol.data[polar])
+        if dipole in mol.data:
+            props[dipole] = float(mol.data[dipole])
+        if polar_exp in mol.data:
+            props[polar_exp] = float(mol.data[polar_exp])
+        return props
+    
+    def create_sol_props_df(self):
+        import pandas as pd
+
+        solprops = {}
+        for sol in self.solutes:
+            solprops[int(sol)] = self.filtering(solute=sol)[0]
+        solprops = pd.DataFrame(solprops).T
+        solprops['solute_dipole'] = solprops['dipole moment']
+        solprops['solute_polari'] = solprops['mean polarizability']
+        solprops['solute_polari_exp'] = solprops['experimental polarizability']
+        solprops = pd.DataFrame(data=solprops, columns=['solute_dipole', 'solute_polari', 'solute_polari_exp'])
+        
+        return solprops
+
+    def filtering(self, solvent=None, solute=None, disp=True):
+
+        if solute != None and solvent != None:
+            return self.__filter_solvent_solute(solvent.upper(), solute.upper(), disp=disp)
+        elif solute != None:
+            return self.__filter_solute(solute.upper())
+        elif solvent != None:
+            return self.__filter_solvent(solvent.upper(), disp=disp)
+
+    def add_dielectric_constant(self, molecule, value, source=None):
+	
+        self.add_property(molecule, 'dielectric constant', value)
+        print('Added dielectric constant epsilon = %f for solute %s' %(value, molecule))
+
+        if source is not None:
+            self.add_property(molecule, 'dielectric constant source', source)
+            print('Added %s as source for dielectric constant for solute %s' %(source, molecule))
+
+    def add_property(self, molecule, prop_name, prop_value, name='database.sdf'):
+	
+        if type(molecule) != str:
+            raise TypeError('Molecule has to be given as a string')
+
+        molecule = self.__name_id_handler([molecule])[0]
+
+        output = pybel.Outputfile('sdf', pathjoin(self.path, name), overwrite=True)
+
+        for mol in self.__sdf_file:
+            if molecule == mol.title:
+                mol.data[prop_name] = prop_value
+            output.write(mol)
+
+        output.close()
+
+    def seperate(self):
+        """
+        Seperates the whole SD file into a file per solute
+        """
+
+        import time
+
+        time = time.strftime("%Y%m%d%H%M%S")
+        folder = 'seperated_files_' + time
+        os.mkdir(folder)
+        os.chdir(folder)
+
+        for mol in self.__sdf_file:
+            singlefile = pybel.Outputfile("sdf", str(mol.title) + '.sdf', overwrite=True)
+            singlefile.write(mol)
+            singlefile.close()
+
+    def draw(self, molecule, title=None, notebook=False, save=None):
+        """
+        Drawing a given molecules. 
+        
+        If using this method in jupyter notebooks, one can set 'notebook=True' for inline printing.
+        
+        Per default the database ID and the molecule name are printed as title.
+        For printing another title use the key word 'title'.
+        For printing no title, set 'title='' '.
+        
+        For saving the figure use 'save=<folder name>'. The folder will be created in the current directory.
+        The picture will be saved as a png.
+        """
+        molecule = self.__name_id_handler([molecule])
+
+        displays = []
+        for mol in molecule:
+            mol = self.__sdf_file[int(mol)]
+            
+            oldtitle = mol.title
+            if title is not None:
+                mol.title = title
+            else:
+                mol.title = mol.title + ' ' + self.solutes[mol.title]
+            if notebook:
+                from IPython.display import Image
+                mol.draw(show=False, filename='temp.png')
+                display = Image('temp.png')
+                
+                if save:
+                    try:
+                        os.mkdir(save)
+                    except OSError:
+                        pass
+                    os.rename('temp.png', pathjoin(save, mol.title.replace(' ','_') + '.png'))
+                    print 'Picture saved as ', pathjoin(save, mol.title.replace(' ','_') + '.png')
+                else:
+                    os.remove('temp.png')
+                mol.title = oldtitle
+                return display
+        
+            else:
+                if save:
+                    try:
+                        os.mkdir(save)
+                    except OSError:
+                        pass
+                    mol.draw(show=False, filename=pathjoin(save, mol.title.replace(' ','_') + '.png'))
+                    print 'Picture saved as ', pathjoin(save, mol.title.replace(' ','_') + '.png')
+                else:
+                    mol.draw()
+
+            mol.title = oldtitle
