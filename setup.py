a4f328fc8ed999da9a0e763b2db3f3cdde7fe38e
Christoph Hille <c.hille@tum.de>
2018-08-20 18:18:59 +0200

    really last chance in setup.py before the first release

--- setup.py
+++ setup.py
@@ -1,27 +1,48 @@
 #!/usr/bin/env python
+#
+# Copyright (C) 2018 Christoph Hille
 
+DESCRIPTION = "Solv@TUM - The Solvation Free Energy Database"
+LONG_DESCRIPTION = """\
+With this module you can interact with the Solv@TUM database. 
+The usage of this database interface is described in the README. 
+
+Please note: This module was written for Python=2.7. 
+Currently there is no compatibility with Python=3.*.
+"""
+
+DISTNAME = 'solvatum'
+MAINTAINER = 'Christoph Hille'
+MAINTAINER_EMAIL = 'c.hille@tum.de'
+LICENSE = 'CC-BY-SA'
+DOWNLOAD_URL = 'https://mediatum.ub.tum.de/1452573'
+VERSION = '1.0.0'
+
+INSTALL_REQUIRES = ['openbabel>=2.4.0']
+
+PACKAGES = ['solvatum']
+        
 import sys
 from setuptools import setup
-from solvatum import __version__, __author__, __email__
 
-try:
-    import pybel
-except ImportError:
-    sys.exit(u"You have to install the Open Babel chemistry library and the Python interface 'pybel' before you can install the Solv@TUM database interface. See the README for more informations.")
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-    
-setup(
-    name='solvatum',
-    version=__version__,
-    packages=['solvatum'],
-    include_package_data=True,
-    author=__author__,
-    author_email=__email__,
-    description='Solvation Free Energy Database',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://mediatum.ub.tum.de/1452573',
-    license='CC-BY-SA',
-)
+if __name__ ==  "__main__":
+    try:
+        import pybel
+    except ImportError:
+        sys.exit(u"You have to install the Open Babel chemistry library and the Python interface 'pybel' before you can install the Solv@TUM database interface. See the README for more informations.") 
+        
+    setup(
+        name=DISTNAME,
+        author=MAINTAINER,
+        author_email=MAINTAINER_EMAIL,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        license=LICENSE,
+        version=VERSION,
+        download_url=DOWNLOAD_URL,
+        packages=PACKAGES,
+        include_package_data=True,
+        python_requires='>=2.7, !=3.*',
+        )
--- solvatum/__init__.py
+++ solvatum/__init__.py
@@ -1,14 +1,2 @@
-"""
-With this module you can interact with the Solv@TUM database.
-The usage of this database interface is described in the README.
 
-Please note:
-This module was written for Python2.7.
-The compatibility with Python3 has not been tested.
-
-Last Update 18.08.2018
-"""
-
-__version__ = '1.0'
-__author__ = 'Christoph Hille'
-__email__ = 'c.hille@tum.de'
+__version__ = "1.0.0"
